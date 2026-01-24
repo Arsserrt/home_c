e300ae49182e4dcb96b10b4332e8ad9eec91e512
Sergey Arsenyev <arsserrt@yandex.ru>
2026-01-24 18:16:59 +0300

    hw1

---
+++ hw1/Let'sgowalk.c
@@ -0,0 +1,15 @@
+/*
+ * идем гулять
+ */
+
+
+#include <stdio.h>
+
+int main(int argc, char **argv)
+{
+	printf("Let's\n");
+	printf("   go\n");
+	printf("     to walk\n");
+	return 0;
+}
+
---
+++ hw1/home.c
@@ -0,0 +1,18 @@
+/*
+ *домик
+ */
+
+
+#include <stdio.h>
+
+int main(int argc, char **argv)
+{
+	printf("   *\n");
+	printf("  ***\n");
+	printf(" *****\n");
+	printf("*******\n");
+	printf(" HHOHH\n");
+	printf(" ZZZZZ\n");
+	return 0;
+}
+
